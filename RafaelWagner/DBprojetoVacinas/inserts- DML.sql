ba3ff7b4c5465663ff6725f46237ba3801729c78
rafael.silva5 <rafael.silva5@FLE207D09.alunos.sc.senac.br>
2021-03-31 11:23:24 -0300

    continuacao commint anterior

--- RafaelWagner/DBprojetoVacinas/inserts- DML.sql
+++ RafaelWagner/DBprojetoVacinas/inserts- DML.sql
@@ -1,30 +1,6 @@
 USE DBVACINACAO;
 
- /*inserts vacina*/
-insert into vacina 
-	(NOME 
-    ,INICIO_PESQUISA 
-    ,PESQUISADOR
-    ,ESTAGIO)
-    value 
-    ( 'gama'
-    , '2020-02-05'
-    , 'paulo'
-    , 'APLICACAO_EM_MASSA'
-    );
-    
-    insert into vacina 
-	(NOME 
-    ,INICIO_PESQUISA 
-    ,PESQUISADOR
-    ,ESTAGIO)
-    value 
-    ( 'alpha'
-    , '2015-08-11'
-    , 'JOAO'
-    , 'INICIAL'
-    );
-    
+
     /*insert pessoa*/
     
     insert into pessoa 
@@ -34,43 +10,58 @@ insert into vacina
     , SEXO
     , categoria
     )
-    VALUE 
+    VALUES 
     ( '48632156988'
 	, 'ALBERTO'
     , '1990-02-02'
     , 'M'
     , 'PESQUISADOR'
-    );
-    
-    insert into pessoa 
-    (CPF 
-    , NOME 
-    , NASCIMENTO 
-    , SEXO
-    , categoria
     )
-    VALUES 
-    ( '58963214722'
+    
+    ,( '58963214722'
 	, 'JOANA'
     , '1985-02-02'
     , 'F'
     , 'VOLUNTARIO'
-    );
+    )
     
-     insert into pessoa 
-    (CPF 
+    ,(CPF 
     , NOME 
     , NASCIMENTO 
     , SEXO
     , categoria
     )
-    VALUES
-    ( '69848963211'
+   
+   , ( '69848963211'
 	, 'MARIA'
     , '1976-07-09'
     , 'F'
     , 'PUBLICO_GERAL'
     );
+    
+     /*inserts vacina*/
+insert into vacina 
+	(NOME 
+    ,INICIO_PESQUISA 
+    ,IDPESQUISADOR
+    ,ESTAGIO
+    ,nome_pais_origem
+    )
+    values
+    ( 'gama'
+    , '2020-02-05'
+    , 1
+    , 'APLICACAO_EM_MASSA'
+    , 'Jamaica'
+    )
+    
+    ,( 'alpha'
+    , '2015-08-11'
+    , 1
+    , 'INICIAL'
+    , 'brasil'
+    );
+    
     
     /*inserts aplicacoes*/
     
@@ -83,18 +74,15 @@ insert into vacina
     '2020-03-11'
     , 1
     , 3
-    );
-    
-	INSERT INTO APLICACAO 
+    )
+    ,
     (
-    DT_APLICACAO
-    , IDVACINA 
-    , IDPESSOA 
-    ) VALUES (
     '2018-01-11'
     , 2
     , 2
     );
+    
+    -- INSERTS AVALIAÇÕES
     
     INSERT INTO AVALIACAO_APLICACAO
     (
@@ -110,8 +98,12 @@ insert into vacina
         , 2
     );
 
+/*
 select * from vacina;
 select * from pessoa;	
 select * from APLICACAO;
 select * from AVALIACAO_APLICACAO;
 SELECT * FROM PESQUISADOR_VACINA;
+*/
+
+select * from vacina where nome= 'GaMa';
